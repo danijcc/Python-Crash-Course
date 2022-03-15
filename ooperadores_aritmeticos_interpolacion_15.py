daa68e5b8e3190709d7b91bd1ae850ce22a9ff26
Dani <danicolmenares7@gmail.com>
2022-03-14 23:35:05 -0500

    Operadores-aritmeticos-ejercicios

---
+++ ooperadores_aritmeticos_interpolacion_15.py
@@ -0,0 +1,15 @@
+#addition         +
+#subtraction      -
+# multiplication  *
+#division         /
+#integer division //
+#exponention      **
+#modulo/remainder %
+
+operandoA = 3
+operandoB = 2
+suma = operandoA + operandoB
+print('Resultado de la suma ',suma )
+#otra forma de imprimir es con f literal a esto se le 
+#llama interpolacion (incluir variables en una cadena)
+print(f'Resultado de la suma: {suma} ')
---
+++ ñejercicio_input_14.py
@@ -0,0 +1,31 @@
+#ejercico califica tu dia, preguntarle al usuario como  estuvo su dia e3 1 al  10
+#lo que hice yo=
+comoEstuoMiDia = (input("como estubo tu dia de 1 al 10: "))
+miDiaEstuvo = "Mi dia estubo de:"
+resultado = miDiaEstuvo + comoEstuoMiDia
+print(resultado)
+
+#la solucion del curso=
+resultado = input("como estuvo tu dia?: ")
+print("mi dia estuvo: ", resultado)
+
+# Se solicita incluir la siguiente información acerca de un libro:
+
+# titulo
+
+# autor
+
+# Debes imprimir la información en el siguiente formato:
+
+# Proporciona el título:
+# Proporciona el autor:
+# <titulo> fue escrito por <autor>
+
+#mi codigo
+tituloDelLibro = input("Proporciona el título:")
+autorDelLibro = input("Proporciona el autor:")
+print(tituloDelLibro, "fue escrito", autorDelLibro)
+#el del curso
+titulo = input("Proporciona el titulo del libro:")
+autor = input("Proporciona el autor del libro:")
+print(titulo, "fue escrito por", autor)
