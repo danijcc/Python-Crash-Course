4b2e30547390a3d2047e748e2130a10cfb5b9135
Dani <danicolmenares7@gmail.com>
2022-03-21 16:06:37 -0500

    Interpolacion-ejercicio

---
+++ ptarea_Interpolacion_16.py
@@ -0,0 +1,20 @@
+# Se solicita incluir la siguiente información acerca de un libro:
+
+# titulo
+
+# autor
+
+# Debes imprimir la información en el siguiente formato:
+
+# Proporciona el título:
+# Proporciona el autor:
+# <titulo> fue escrito por <autor>
+# Nota: Recordar que las tareas no cambian de estado y no afectan en el avance de tu curso ni en la generación de tu certificado de finalización del curso en Udemy.
+
+# Preguntas de esta tarea
+# ¿Cuál es el código del requerimiento solicitado?
+# mi respuesta aplicando f literal 
+titulo = input('Proporciona el título:')
+autor = input('Proporciona el autor:')
+resultado = (f'{titulo} fue escrito por {autor}')
+print(resultado)
