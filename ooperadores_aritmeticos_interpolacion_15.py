7ddeeba7d4db15fa1cfb36f1e233890ff3316eb6
Dani <danicolmenares7@gmail.com>
2022-03-21 15:53:15 -0500

    IInterpolacion

--- ooperadores_aritmeticos_interpolacion_15.py
+++ ooperadores_aritmeticos_interpolacion_15.py
@@ -2,9 +2,9 @@
 #subtraction      -
 # multiplication  *
 #division         /
-#integer division //
+#integer division // no utiliza punto flotante regresa un valor entero siempre
 #exponention      **
-#modulo/remainder %
+#modulo/remainder % resuduo de la division
 
 operandoA = 3
 operandoB = 2
@@ -13,3 +13,15 @@ print('Resultado de la suma ',suma )
 #otra forma de imprimir es con f literal a esto se le 
 #llama interpolacion (incluir variables en una cadena)
 print(f'Resultado de la suma: {suma} ')
+resta = operandoA - operandoB
+print(f'resultado resta: {resta}')
+multiplicacion = operandoA * operandoB
+print(f'Resultado multiplicacion {multiplicacion}')
+division = operandoA / operandoB
+print(f'Resultado division {division}')
+divisionint = operandoA // operandoB
+print(f'Resultado division enteros: {divisionint}')
+modulo = operandoA % operandoB
+print(f'Resultado residuo division (modulo): {modulo}')
+exponente = operandoA ** operandoB
+print(f'Resultado exponente: {exponente}')
