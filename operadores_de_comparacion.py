585a36353e6056278cb3b85133ed1883b876a417
Dani <danicolmenares7@gmail.com>
2022-03-26 13:49:42 -0500

    Operadores-de-comparacion

---
+++ operadores_de_comparacion.py
@@ -0,0 +1,26 @@
+a = 4
+b = 2
+
+#esto evalua y (a) es exactamente igual a (b)
+resultado = a == b
+print(f'resultado == : {resultado}')
+
+#evalua si (a) es diferente de (b)
+resultado = a != b
+print(f'resultado != : {resultado}')
+
+#evalua si (a) es mayor que (b)
+resultado = a > b
+print(f'Resultado > : {resultado}')
+
+#evalua si (a) es mayor igual que (b)
+resultado = a >= b
+print (f'Resultado >= :{resultado}')
+
+#evalua si (a) menor que (b)
+resultado = a < b
+print(f'Resultado < : {resultado}')
+
+#evalua si (a) es menor o igual que (b)
+resultado = a <= b
+print(f'Resultado <= : {resultado}')
