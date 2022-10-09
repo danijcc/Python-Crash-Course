e3397f0e4bd3a80e7638fc8f869172df6145f7c1
danijcc <danicolmenares7@gmail.com>
2022-10-09 11:55:47 -0500

    main

--- 5_main.py
+++ 5_main.py
@@ -2,7 +2,9 @@
 #doc https://kivymd.readthedocs.io/en/latest/
 #doc https://kivymd.readthedocs.io/en/0.104.1/index.html
 #doc https://kivymd.readthedocs.io/en/0.104.2/components/text-field/index.html
+from importlib.metadata import entry_points
 from turtle import title
+from typing import Text
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.dialog import MDDialog
@@ -10,6 +12,8 @@ from kivymd.uix.button import MDFlatButton
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen
+from kivy.uix.textinput import TextInput
 #set windows size
 Window.size=(500,800)
 
@@ -24,24 +28,30 @@ Screen:
             height: self.minimum_height
             MDCard:
                 size_hint: None, None
-                size:500,1000
-                pos_hint: {"center_x": 0.5, "center_y": 0.6}
+                size:500,1330
+                pos_hint: {"center_x": 0.5, "center_y": 0.5}
                 elevation: 10
                 padding: 50
                 spacing: 35
                 orientation : 'vertical'
+                multiline: False
                 MDIcon:
                     icon: 'engine'
-                    icon_color: 0,0,0,0
+                    icon_color: 1,0,0,0
                     halign: 'center'
                     font_size: 130
                     pos_hint: {"center_x": 0.5, "center_y": 0.5}
                 MDLabel:
-                    text: "<<Calcular Relacion de Compresion>>" 
+                    text: "Calcular Relacion de Compresion." 
                     id: text
                     icon_left: "circle-slice-4"
-                    width: 220
+                    width: 230
                     font_size: 20
+                    halign: 'center'
+                MDLabel:
+                    text: 'Utilizar puntos no comas para expresar los decimales                                   ejemplo 4.250" en vez de 4,250"' 
+                    width: 220
+                    font_size: 11
                     halign: 'center'
                 MDTextField:
                     id: valor1
@@ -127,7 +137,7 @@ Screen:
                     size_hint_x: None
                     width: 220
                     font_size: 20
-                    pos_hint: {"center_x": 0.3}
+                    pos_hint: {"center_x": 0.5}
                     mode: "fill"
                     fill_color: 0, 0, 0, .4
                     size_hint_x: .9
@@ -136,13 +146,13 @@ Screen:
                 MDTextField:
                     id: valor5
                     icon_left: "arrow-split-horizontal"
-                    hint_text: "Volumen de la corona o crown de la cabeza del piston"
+                    hint_text: "Volumen de la corona de la cabeza del piston"
                     helper_text: "Ingrese este dato en Centimetros Cubicos"
                     foreground_color: 1,0,1,1
                     size_hint_x: None
-                    width: 220
+                    width: 150
                     font_size: 20
-                    pos_hint: {"center_x": 0.3}
+                    pos_hint: {"center_x": 0.5}
                     mode: "fill"
                     fill_color: 0, 0, 0, .4
                     size_hint_x: .9
@@ -154,7 +164,73 @@ Screen:
                     pos_hint: {"center_x": 0.5}
                     on_press: app.garage()
                 MDLabel:
-                    text: "<<---Design by Dani Colmenares--->>" 
+                    text: "#################################" 
+                    width: 220
+                    font_size: 20
+                    halign: 'center'
+                MDLabel:
+                    id: volumen1
+                    icon_left: "circle-slice-4"
+                    width: 220
+                    font_size: 12
+                    halign: 'left'
+                    hint_text: "required = True"  
+                    multiline: False
+                MDLabel:
+                    id: volumen2
+                    icon_left: "circle-slice-4"
+                    width: 220
+                    font_size: 12
+                    halign: 'left'
+                    hint_text: "required = True" 
+                    multiline: False
+                MDLabel:
+                    id: volumen3
+                    icon_left: "circle-slice-4"
+                    width: 220
+                    font_size: 12
+                    halign: 'left'
+                    hint_text: "required = True" 
+                    multiline: False
+                MDLabel:
+                    id: volumen4
+                    icon_left: "circle-slice-4"
+                    width: 220
+                    font_size: 12
+                    halign: 'left'
+                    hint_text: "required = True" 
+                    multiline: False
+                MDLabel:
+                    id: volumen5
+                    icon_left: "circle-slice-4"
+                    width: 220
+                    font_size: 12
+                    halign: 'left'
+                    hint_text: "required = True"
+                    multiline: False
+                MDLabel:
+                    text: "#################################" 
+                    width: 220
+                    font_size: 20
+                    halign: 'center'
+                MDLabel:
+                    id: plano
+                    icon_left: "circle-slice-4"
+                    width: 220
+                    font_size: 12
+                    halign: 'left'
+                    hint_text: "required = True" 
+                    multiline: False
+                MDLabel:
+                    id: domo
+                    icon_left: "circle-slice-4"
+                    width: 220
+                    font_size: 12
+                    halign: 'left'
+                    hint_text: "required = True"  
+                    multiline: False
+                MDLabel:
+                    text: "#################################<<------desing by Dani Colmenares------>>" 
                     id: text
                     icon_left: "circle-slice-4"
                     width: 220
@@ -173,13 +249,15 @@ Screen:
 
 '''
 
-class GarageApp(MDApp):
+class Garage7App(MDApp):
     
 
     def build(self):
         return Builder.load_string(KV)
 
     def garage(self):
+        #almacenar el valor del input del usuario en una variable donde valor.text
+        #es igual al id de MDTextField:
         valor1 = self.root.ids.valor1.text
         stroke = self.root.ids.stroke.text
         valor2 = self.root.ids.valor2.text
@@ -188,7 +266,7 @@ class GarageApp(MDApp):
         valor4 = self.root.ids.valor4.text
         valor5 = self.root.ids.valor5.text
 
-
+        
         v1 = float(valor1) * float(valor1) * float(stroke)* 0.7854
         v2 = float(valor2) * 1 / 16.3871 
         v3 = float(valor6) * float(valor6) * float(valor3) * 0.7854
@@ -210,6 +288,7 @@ class GarageApp(MDApp):
         VOLUMEN CABEZA DEL PISTON:.......................................[{v5}]
         ############################################################################
         ''')
+        
 
         volumen_de_la_camara_plano =  v2+v3+v4+v5
         volumen_del_cilindro_plano =  v1+v2+v3+v4+v5 
@@ -229,12 +308,30 @@ class GarageApp(MDApp):
         ############################################################################
 
         ''')
-
+        #convertir a str los valores resultante ya que las etiqueta solo admite str y
+        #concatenar valores con sus respectivos resultados
+        v1 = 'VOLUMEN DEL CILINDRO:...............'+str(v1)
+        v2 = 'VOLUMEN DE LA CAMARA:..............'+str(v2)
+        v3 = 'VOLUMEN DE LA EMPACADURA:..........'+str(v3)
+        v4 = 'VOLUMEN ENTRE PISTON Y PUNTO MUERTO SUPERIOR:.........'+str(v4)
+        v5 = 'VOLUMEN CABEZA DEL PISTON:.........'+str(v5)
+        resultado_piston_plano = 'RELACION DE COMPRESION PARA PISTON FLAT O PLANO:..........'+str(resultado_piston_plano) + ' A 1'
+        resultado_piston_domo = 'RELACION DE COMPRESION PARA PISTON DOME O DOMO:...........'+str(resultado_piston_domo) + ' A 1'
+  
+        #mostrar el resultado almacenado en las variables en una etiqueta label MDLabel:  
+        #donde valor.text es valor del id: de la etiqueta 
+        self.root.ids.volumen1.text = v1
+        self.root.ids.volumen2.text = v2
+        self.root.ids.volumen3.text = v3
+        self.root.ids.volumen4.text = v4
+        self.root.ids.volumen5.text = v5
+        self.root.ids.plano.text = resultado_piston_plano
+        self.root.ids.domo.text = resultado_piston_domo
 
     def close(self):
         self.dialog.dismiss()
-        
-GarageApp().run()        
+
+Garage7App().run()        
                 
             
 
